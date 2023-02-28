515a8b0d641a5a4908f40adc536a4c66583f102f
GiulianaBCastillo <giuliana.b.castillo@gmail.com>
2023-02-28 08:33:34 +0000

    functions created to render the license section of the README and to render the license link

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,3 +1,28 @@
+// function that returns a license badget
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return`![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+  }
+  return "";
+}
+
+// function that returns the license link
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `\n* [License](#license)\n`;
+  }
+  return "";
+}
+
+//function that returns the license section of the README
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `##License
+    Licensed under the ${license} license.`;
+  }
+  return "";
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
